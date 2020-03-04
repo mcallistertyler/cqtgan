b081d2940311410f03a54d674eb1a7566a2bd175
mcallistertyler95 <tytheguy95@gmail.com>
2020-03-04 16:10:44 +0100

    Cleaned up code for hacky inference and improved wav-to-cqt.py

--- scripts/train.py
+++ scripts/train.py
@@ -106,61 +106,16 @@ def main():
     #######################
     # Create data loaders #
     #######################
-    if not inference:
-        train_set = AudioDataset(
-            Path(args.data_path) / "train_files.txt", args.seq_len, sampling_rate=22050
-        )
-        test_set = AudioDataset(
-            Path(args.data_path) / "test_files.txt",
-            22050 * 4,
-            sampling_rate=22050,
-            augment=False,
-        )
-
-        train_loader = DataLoader(train_set, batch_size=args.batch_size, num_workers=4)
-        test_loader = DataLoader(test_set, batch_size=1)
-    else:
-        print('Inference started')
-        # sample_set = AudioDataset(
-        #     Path(args.data_path) / "infer_files.txt",
-        #     22050 * 4,
-        #     sampling_rate=22050,
-        #     augment=False,
-        # )
-        # sample_loader = DataLoader(sample_set, batch_size=1)
-
-    # Generate audio from supplied spectrogram
     if inference:
-        # sample_voc = []
-        # sample_audio = []
-        # for i, x_t in enumerate(test_loader):
-        #     x_t = x_t.cuda()
-        #     s_t = fft(x_t).detach()
-
-        #     sample_voc.append(s_t.cuda())
-        #     sample_audio.append(x_t)
-
-        #     audio = x_t.squeeze().cpu()
-        #     save_sample(root / ("original_%d.wav" % i), 22050, audio)
-        #     writer.add_audio("original/sample_%d.wav" % i, audio, 0, sample_rate=22050)
-
-        #     if i == args.n_test_sample - 1:
-        #         break
+        print('Starting inference')
         st = time.time()
         with torch.no_grad():
-            #image = Image.open(args.test_spec)
-            #print('image opened', image)
-            #x = TF.to_tensor(image).cuda()
-            #print('x to tf tensor', x.shape)
             x = torch.load('unseen.pt')
-            print('god has entered the chatroom', x.shape)
             save_spec_images(x)
             x = x.cuda()
-            print('x shape', x.shape)
             pred_audio = netG(x)
             pred_audio = pred_audio.squeeze().cpu()
-            print('audio shape', pred_audio.shape)
-            save_sample(root / ("unseenboys2.wav"), 22050, pred_audio)
+            save_sample(root / ("generated_sample.wav"), 22050, pred_audio)
             writer.add_audio(
                 "generated/sample_test.wav",
                 pred_audio,
@@ -168,145 +123,145 @@ def main():
                 sample_rate=22050,
             )
             print('Finished inference')
-            # for i, (voc, _) in enumerate(zip(sample_voc, sample_audio)):
-            #     pred_audio = netG(voc)
-            #     pred_audio = pred_audio.squeeze().cpu()
-            #     save_sample(root / ("generated_%d.wav" % i), 22050, pred_audio)
-            #     writer.add_audio(
-            #         "generated/sample_%d.wav" % i,
-            #         pred_audio,
-            #         global_step=None,
-            #         sample_rate=22050,
-            #     )
-            # print('Finished inference')
-    else:
-        ##########################
-        # Dumping original audio #
-        ##########################
-        test_voc = []
-        test_audio = []
-        for i, x_t in enumerate(test_loader):
+        return
+        
+    train_set = AudioDataset(
+        Path(args.data_path) / "train_files.txt", 
+        args.seq_len, 
+        sampling_rate=22050)
+
+    test_set = AudioDataset(
+        Path(args.data_path) / "test_files.txt",
+        22050 * 4,
+        sampling_rate=22050,
+        augment=False)
+
+    train_loader = DataLoader(train_set, batch_size=args.batch_size, num_workers=4)
+    test_loader = DataLoader(test_set, batch_size=1)
+
+    ##########################
+    # Dumping original audio #
+    ##########################
+    test_voc = []
+    test_audio = []
+    for i, x_t in enumerate(test_loader):
+        x_t = x_t.cuda()
+        s_t = fft(x_t).detach()
+
+        test_voc.append(s_t.cuda())
+        test_audio.append(x_t)
+
+        audio = x_t.squeeze().cpu()
+        save_sample(root / ("original_%d.wav" % i), 22050, audio)
+        writer.add_audio("original/sample_%d.wav" % i, audio, 0, sample_rate=22050)
+
+        if i == args.n_test_samples - 1:
+            break
+    costs = []
+    start = time.time()
+    # enable cudnn autotuner to speed up training
+    torch.backends.cudnn.benchmark = True
+
+    best_mel_reconst = 1000000
+    steps = 0
+    for epoch in range(1, args.epochs + 1):
+        for iterno, x_t in enumerate(train_loader):
             x_t = x_t.cuda()
-            s_t = fft(x_t).detach()
+            s_t = fft(x_t).detach() # generate spectrogram
+            x_pred_t = netG(s_t.cuda()) # generate audio from real spectrogram
+            with torch.no_grad():
+                s_pred_t = fft(x_pred_t.detach()) # get spectrogram from the audio we generated
+                s_error = F.l1_loss(s_t, s_pred_t).item() # find loss between generated spectrogram from real audio and the spectrogram from audio by the generator
+            #######################
+            # Train Discriminator #
+            #######################
+            D_fake_det = netD(x_pred_t.cuda().detach())
+            D_real = netD(x_t.cuda())
 
-            test_voc.append(s_t.cuda())
-            test_audio.append(x_t)
+            loss_D = 0
+            for scale in D_fake_det:
+                loss_D += F.relu(1 + scale[-1]).mean()
 
-            audio = x_t.squeeze().cpu()
-            save_sample(root / ("original_%d.wav" % i), 22050, audio)
-            writer.add_audio("original/sample_%d.wav" % i, audio, 0, sample_rate=22050)
+            for scale in D_real:
+                loss_D += F.relu(1 - scale[-1]).mean()
 
-            if i == args.n_test_samples - 1:
-                break
-        costs = []
-        start = time.time()
-        # enable cudnn autotuner to speed up training
-        torch.backends.cudnn.benchmark = True
+            netD.zero_grad()
+            loss_D.backward()
+            optD.step()
 
-        best_mel_reconst = 1000000
-        steps = 0
-        for epoch in range(1, args.epochs + 1):
-            for iterno, x_t in enumerate(train_loader):
-                x_t = x_t.cuda()
-                s_t = fft(x_t).detach() # generate spectrogram
-                x_pred_t = netG(s_t.cuda()) # generate audio from real spectrogram
+            ###################
+            # Train Generator #
+            ###################
+            D_fake = netD(x_pred_t.cuda())
+
+            loss_G = 0
+            for scale in D_fake:
+                loss_G += -scale[-1].mean()
+
+            loss_feat = 0
+            feat_weights = 4.0 / (args.n_layers_D + 1)
+            D_weights = 1.0 / args.num_D
+            wt = D_weights * feat_weights
+            for i in range(args.num_D):
+                for j in range(len(D_fake[i]) - 1):
+                    loss_feat += wt * F.l1_loss(D_fake[i][j], D_real[i][j].detach())
+
+            netG.zero_grad()
+            (loss_G + args.lambda_feat * loss_feat).backward()
+            optG.step()
+
+            ######################
+            # Update tensorboard #
+            ######################
+            costs.append([loss_D.item(), loss_G.item(), loss_feat.item(), s_error])
+
+            writer.add_scalar("loss/discriminator", costs[-1][0], steps)
+            writer.add_scalar("loss/generator", costs[-1][1], steps)
+            writer.add_scalar("loss/feature_matching", costs[-1][2], steps)
+            writer.add_scalar("loss/mel_reconstruction", costs[-1][3], steps)
+            steps += 1
+
+            if steps % args.save_interval == 0:
+                st = time.time()
                 with torch.no_grad():
-                    s_pred_t = fft(x_pred_t.detach()) # get spectrogram from the audio we generated
-                    s_error = F.l1_loss(s_t, s_pred_t).item() # find loss between generated spectrogram from real audio and the spectrogram from audio by the generator
-                #######################
-                # Train Discriminator #
-                #######################
-                D_fake_det = netD(x_pred_t.cuda().detach())
-                D_real = netD(x_t.cuda())
-
-                loss_D = 0
-                for scale in D_fake_det:
-                    loss_D += F.relu(1 + scale[-1]).mean()
-
-                for scale in D_real:
-                    loss_D += F.relu(1 - scale[-1]).mean()
-
-                netD.zero_grad()
-                loss_D.backward()
-                optD.step()
-
-                ###################
-                # Train Generator #
-                ###################
-                D_fake = netD(x_pred_t.cuda())
-
-                loss_G = 0
-                for scale in D_fake:
-                    loss_G += -scale[-1].mean()
-
-                loss_feat = 0
-                feat_weights = 4.0 / (args.n_layers_D + 1)
-                D_weights = 1.0 / args.num_D
-                wt = D_weights * feat_weights
-                for i in range(args.num_D):
-                    for j in range(len(D_fake[i]) - 1):
-                        loss_feat += wt * F.l1_loss(D_fake[i][j], D_real[i][j].detach())
-
-                netG.zero_grad()
-                (loss_G + args.lambda_feat * loss_feat).backward()
-                optG.step()
-
-                ######################
-                # Update tensorboard #
-                ######################
-                costs.append([loss_D.item(), loss_G.item(), loss_feat.item(), s_error])
-
-                writer.add_scalar("loss/discriminator", costs[-1][0], steps)
-                writer.add_scalar("loss/generator", costs[-1][1], steps)
-                writer.add_scalar("loss/feature_matching", costs[-1][2], steps)
-                writer.add_scalar("loss/mel_reconstruction", costs[-1][3], steps)
-                steps += 1
-
-                if steps % args.save_interval == 0:
-                    st = time.time()
-                    with torch.no_grad():
-                        for i, (voc, _) in enumerate(zip(test_voc, test_audio)):
-                            save_spec_images(voc)
-                            print('shape of voc', voc.shape)
-                            print('saved spec')
-                            pred_audio = netG(voc)
-                            print('pred audio shape', pred_audio.shape)
-                            pred_audio = pred_audio.squeeze().cpu()
-                            print('pred audio squeezed shape', pred_audio.shape)
-                            save_sample(root / ("generated_%d.wav" % i), 22050, pred_audio)
-                            writer.add_audio(
-                                "generated/sample_%d.wav" % i,
-                                pred_audio,
-                                epoch,
-                                sample_rate=22050,
-                            )
-
-                    torch.save(netG.state_dict(), root / "netG.pt")
-                    torch.save(optG.state_dict(), root / "optG.pt")
-
-                    torch.save(netD.state_dict(), root / "netD.pt")
-                    torch.save(optD.state_dict(), root / "optD.pt")
-
-                    if np.asarray(costs).mean(0)[-1] < best_mel_reconst:
-                        best_mel_reconst = np.asarray(costs).mean(0)[-1]
-                        torch.save(netD.state_dict(), root / "best_netD.pt")
-                        torch.save(netG.state_dict(), root / "best_netG.pt")
-
-                    print("Took %5.4fs to generate samples" % (time.time() - st))
-                    print("-" * 100)
-
-                if steps % args.log_interval == 0:
-                    print(
-                        "Epoch {} | Iters {} / {} | ms/batch {:5.2f} | loss {}".format(
+                    for i, (voc, _) in enumerate(zip(test_voc, test_audio)):
+                        save_spec_images(voc)
+                        pred_audio = netG(voc)
+                        pred_audio = pred_audio.squeeze().cpu()
+                        save_sample(root / ("generated_%d.wav" % i), 22050, pred_audio)
+                        writer.add_audio(
+                            "generated/sample_%d.wav" % i,
+                            pred_audio,
                             epoch,
-                            iterno,
-                            len(train_loader),
-                            1000 * (time.time() - start) / args.log_interval,
-                            np.asarray(costs).mean(0),
+                            sample_rate=22050,
                         )
+
+                torch.save(netG.state_dict(), root / "netG.pt")
+                torch.save(optG.state_dict(), root / "optG.pt")
+
+                torch.save(netD.state_dict(), root / "netD.pt")
+                torch.save(optD.state_dict(), root / "optD.pt")
+
+                if np.asarray(costs).mean(0)[-1] < best_mel_reconst:
+                    best_mel_reconst = np.asarray(costs).mean(0)[-1]
+                    torch.save(netD.state_dict(), root / "best_netD.pt")
+                    torch.save(netG.state_dict(), root / "best_netG.pt")
+
+                print("Took %5.4fs to generate samples" % (time.time() - st))
+                print("-" * 100)
+
+            if steps % args.log_interval == 0:
+                print(
+                    "Epoch {} | Iters {} / {} | ms/batch {:5.2f} | loss {}".format(
+                        epoch,
+                        iterno,
+                        len(train_loader),
+                        1000 * (time.time() - start) / args.log_interval,
+                        np.asarray(costs).mean(0),
                     )
-                    costs = []
-                    start = time.time()
+                )
+                costs = []
+                start = time.time()
 
 
 if __name__ == "__main__":
