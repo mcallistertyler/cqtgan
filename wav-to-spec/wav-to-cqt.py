fba83de30fb6dc780130d54fd778a269b6aa1044
Tyler McAllister <mcallistertyler95@gmail.com>
2020-03-17 18:08:40 +0100

    something broken?

--- wav-to-spec/wav-to-cqt.py
+++ wav-to-spec/wav-to-cqt.py
@@ -3,7 +3,10 @@
 from spec2wav.dataset import AudioDataset
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
+from torchvision.transforms import ToTensor, ToPILImage
+import torch.nn.functional as F
 import argparse
+from PIL import Image
 from nnAudio import Spectrogram
 import torch
 import numpy as np
@@ -14,13 +17,31 @@ from tqdm import tqdm
 min_A = []
 max_A = []
 
+#min: -8.7887
+#max: 2.5628
+
+def inverse_normalisation(min, max):
+    print('inversing normalisation of spec')
+    print('using test values')
+    im = Image.open('hiphop-1-fakeB.png').convert('L')
+    im = ToTensor()(im)
+    tim = torch.load('hiphop-1.pt')
+    print('image', im)
+    print('image shaep', im.shape)
+    print('loaded tensor', tim)
+    denormalised_spec = im[0] * (max - min) + min 
+    print('denormalised', denormalised_spec)
+    print('saving denormalised')
+    torch.save(denormalised_spec, 'denormalised.pt')
+    
 def track_normalisation_average(voc):
     for spec in voc:
-        print('spec', spec)
-
-def save_spec_images(spec, name):
-    imgdt = datetime.datetime.now()
-    plt.imsave('png/' + str(name) + '.png', spec[0].cpu().numpy())
+        for i in enumerate(spec):
+            min_A.append(torch.min(spec[0]))
+            max_A.append(torch.max(spec[0]))
+            print('min a', min_A)
+            print('max a', max_A)
+            inverse_normalisation(-8.7887, 2.5628)
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -32,10 +53,10 @@ def main():
     args = parse_args()
     save_type = args.save_type
     spec_layer = Spectrogram.CQT1992v2(sr=22050, n_bins=84, hop_length=256, pad_mode='constant', device='cuda:0', verbose=False, trainable=False, output_format='Magnitude')
-    transformedSet = AudioDataset('input_audio.txt', 22050 * 4, sampling_rate=22050, augment=False)
+    transformedSet = AudioDataset('hiphop_files.txt', 22050 * 4, sampling_rate=22050, augment=False)
     transformedLoader = DataLoader(transformedSet, batch_size=1)
     transformedVoc = []
-    f = open('input_audio.txt', 'r')
+    f = open('hiphop_files.txt', 'r')
     lines = f.readlines()
     lines = list(map(lambda s: s.strip(), lines)) #remove newline character
     lines = [track.replace('.wav', '') for track in lines] #remove .wav
@@ -52,14 +73,14 @@ def main():
         track_normalisation_average(transformedVoc)
     
     if (save_type == 'test'):
+        print('This is a test')
         return
     if (save_type == 'torch'):
         for x in tqdm(range(0, len(transformedVoc)), ascii=True, desc='Making torch tensors'):
             torch.save(transformedVoc[x], 'torch/' + lines[x] + '.pt')
     if (save_type == 'png'):
-        for x in tqdm(range(0, len(transformedVoc)), ascii=True, desc='Making pngs using matplotlib'):
-            #save_spec_images(transformedVoc[x], lines[x])
-            save_image(transformedVoc[x], lines[x] + '.png', normalize=False)
+        for x in tqdm(range(0, len(transformedVoc)), ascii=True, desc='Making pngs'):
+            save_image(transformedVoc[x], 'png/' + lines[x] + '.png', normalize=True)
 
 if __name__ == "__main__":
     main()
